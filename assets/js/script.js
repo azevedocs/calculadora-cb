ff9e439feef9b5f377a7bfca7ed20425a4694038
Gabriel Azevedo <60318636+azevedocs@users.noreply.github.com>
2024-03-15 19:32:04 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    const adequacao =  ((circunferencia/percentil) * 100).toFixed(2);
+});
