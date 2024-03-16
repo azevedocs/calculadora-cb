3d91dab89e3632b5aabf4c05c59aa75e7a8a41a4
azevedocs <gabriel_azevedocs@outlook.com>
2024-03-16 13:05:06 -0300

    16/03

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event) {
@@ -8,3 +9,29 @@ form.addEventListener('submit', function(event) {
 
     const adequacao =  ((circunferencia/percentil) * 100).toFixed(2);
 });
+
+
+if (sexo === 'feminino') {
+    if (idade >= 1 && idade <= 1.9) {
+            adequacao = (circunferencia/15.7) * 100
+    } else if (idade >= 2 && idade <= 2.9) {
+        adequacao = (circunferencia/16.1) * 100
+    } else if (idade >= 3 && idade <= 3.9) {
+        adequacao = (circunferencia/16.6) * 100
+    } else if (idade >= 4 && idade <= 4.9) {
+        adequacao = (circunferencia/17.0) * 100
+    } else if (idade >= 5 && idade <= 5.9) {
+        adequacao = (circunferencia/17.5) * 100
+    } else if (idade >= 6 && idade <= 6.9) {
+        adequacao = (circunferencia/17.8) * 100
+    } else if (idade >= 7 && idade <= 7.9) {
+        adequacao = (circunferencia/18.6) * 100
+    } else if (idade >= 8 && idade <= 8.9) {
+        adequacao = (circunferencia/19.5) * 100
+    } else if (idade >= 9 && idade <= 9.9) {
+        adequacao = (circunferencia/20.6) * 100
+    }
+
+
+
+}
